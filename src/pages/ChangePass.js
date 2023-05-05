3e2d9b3a23afda6e589c378343fafdfa6c2a6b98
sambeck87 <sandro_mipb@hotmail.com>
2023-05-05 09:04:56 -0600

    Change changepass page view

--- src/pages/ChangePass.js
+++ src/pages/ChangePass.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { changepass } from '../redux/user/userAPI';
 
 const ChangePass = () => {
@@ -39,22 +39,9 @@ const ChangePass = () => {
     <div className="max_width d-flex flex-column back_bg vh-100">
       <div className="max_width d-flex flex-column justify-content-center top_width align-self-center mt-5">
         <img className="logo align-self-center" src="https://thumbs.dreamstime.com/b/rent-icon-189435850.jpg" alt="" />
-        <h2 className="text-center my-5">Do you want to change your password?</h2>
+        <h2 className="text-center my-5">Set your new password?</h2>
         <div className="bg-white rounded-4">
           <form className="my-3" onSubmit={createUser}>
-            <label htmlFor="password" className="max_width px-3 mt-3">
-              Old Password
-              <br />
-              <input
-                className="max_width form-control form-control-lg"
-                type="password"
-                id="password"
-                required
-                onChange={onChange}
-              />
-            </label>
-            <br />
-
             <label htmlFor="password" className="max_width px-3 mt-4">
               Password
               <br />
@@ -88,9 +75,6 @@ const ChangePass = () => {
             <button type="submit" value="add-tour" className="btn primary_bg text-white lg_button">
               Change Password
             </button>
-            <p className="text-center mt-4">
-              <Link to="/login">Login</Link>
-            </p>
           </form>
         </div>
       </div>
