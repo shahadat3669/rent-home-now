1567e3dc88c7c8a09c2f970899205356457bd3dd
sambeck87 <sandro_mipb@hotmail.com>
2023-05-10 12:00:12 -0600

    Create url dynamically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getProperties } from './redux/properties/GetProperties';
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
 import RecoverPass from './pages/RecoverPass';
 import ChangePass from './pages/ChangePass';
 import Details from './pages/Details';
 
-const App = () => (
-  <>
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProperties());
+  }, [dispatch]);
 
-    <Routes>
-      <Route path="/signup" element={<SignUpForm />} />
-      <Route path="/signin" element={<SignInForm />} />
-      <Route path="/recover" element={<RecoverPass />} />
-      <Route path="/change" element={<ChangePass />} />
-      <Route path="/details" element={<Details />} />
-    </Routes>
+  return (
+    <>
+      <Routes>
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/signin" element={<SignInForm />} />
+        <Route path="/recover" element={<RecoverPass />} />
+        <Route path="/change" element={<ChangePass />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/details/:id/" element={<Details />} />
+      </Routes>
 
-  </>
-);
+    </>
+  );
+};
 
 export default App;
