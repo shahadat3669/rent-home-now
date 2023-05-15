cb95dc4bcfb7de29be9d97cc6fe71d2bee264c23
Shahadat Hossain <shahadat3669@gmail.com>
2023-05-15 21:47:19 +0600

    Update user slice and configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,18 @@
 import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import { userReducer } from './user/user';
+import userReducer from './user/userSlice';
 import details from './details/GetDetails';
 import properties from './properties/GetProperties';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    details,
-    properties,
+const store = configureStore(
+  {
+    reducer: {
+      user: userReducer,
+      details,
+      properties,
+    },
   },
-}, applyMiddleware(thunk));
+  applyMiddleware(thunk),
+);
 
 export default store;
