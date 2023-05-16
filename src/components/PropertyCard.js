889e65c88da9c37e0a714556a0386616e1f6fb5c
GuteamG <user.email>
2023-05-16 19:44:59 +0300

    Add rate and address on property image

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { number } from 'prop-types';
 import { FaFacebookF, FaTiktok } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
@@ -24,8 +24,17 @@ export default function PropertyCard({ property }) {
         ............................
       </span>
       <p className="property-description text-center">
-        {property.description}
+        {property.description.substring(0, 30)}
+        ...
       </p>
+      <div className="d-flex justify-content-between w-75">
+        <h5 title="Total Fee">
+          $
+          {property.reservation_criteria.rate
+          + property.reservation_criteria.others_fee}
+        </h5>
+        <h5 title={`State: ${property.address.state}, zip_code: ${property.address.zip_code}`}>{property.address.country}</h5>
+      </div>
       <div className="property-social text-muted">
         <span className="property-social-icon">
           <FaFacebookF color="gray" />
@@ -48,10 +57,20 @@ PropertyCard.propTypes = {
     no_bedrooms: PropTypes.number,
     no_beds: PropTypes.number,
     description: PropTypes.string,
-    user: PropTypes.shape({
+    reservation_criteria: PropTypes.shape({
       id: PropTypes.number,
-      name: PropTypes.string,
-      avatar: PropTypes.string,
+      max_guest: PropTypes.number,
+      others_fee: PropTypes.number,
+      rate: PropTypes.number,
+      min_time_period: number,
+    }),
+    address: PropTypes.shape({
+      id: PropTypes.number,
+      country: PropTypes.string,
+      house_number: PropTypes.string,
+      state: PropTypes.string,
+      street: PropTypes.string,
+      zip_code: PropTypes.string,
     }),
     category: PropTypes.shape({
       id: PropTypes.number,
